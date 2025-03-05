0921709690ff943d919cda975512a4b7de907aac
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-03-05 16:34:04 +0100

    getList method added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-eda32","appId":"1:102185299204:web:2727ef1d2a3ccbe596f451","storageBucket":"danotes-eda32.firebasestorage.app","apiKey":"AIzaSyCrG4mGenIGh9-7Ocg3frwcOkau3b1bZZc","authDomain":"danotes-eda32.firebaseapp.com","messagingSenderId":"102185299204"})), 
+    provideFirestore(() => getFirestore())]
 };
---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
+import { collectionData } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+  
+  // items$;
+  // items;
+
+  unsubTrash;
+  unsubNotes;
+  //unsubSingle;
+
+  firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+    
+
+
+    // this.items$ = collectionData(this.getNotesRef());
+    // this.items = this.items$.subscribe( (list) => {
+    //   list.forEach(element => {
+    //     console.log(element);
+    //   });
+    // } )
+    
+  }
+
+  ngonDestroy(){
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+getsingleDocRef(colId: string, docId: string) {
+  return doc(collection(this.firestore, colId), docId);
+}
+
+setNoteObject(obj: any, id: string): Note{
+  return {
+    id: id || "",
+    type: obj.type || "note",
+    title: obj.title || "",
+    content: obj.content || "",
+    marked: obj.marked || false,
+  }
+}
+
+}
